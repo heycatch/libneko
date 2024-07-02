1c47a3af71990a7b2192e03292b4d246c308ef0b
armv9 <48624112+arm64v8a@users.noreply.github.com>
2024-07-02 11:49:04 +0900

    update speedtest

--- speedtest/speedtest.go
+++ speedtest/speedtest.go
@@ -2,29 +2,47 @@ package speedtest
 
 import (
 	"context"
+	"crypto/tls"
+	"errors"
 	"fmt"
 	"net"
 	"net/http"
-	"strings"
+	"net/http/httptrace"
 	"time"
 )
 
-func UrlTest(client *http.Client, link string, timeout int32) (int32, error) {
+const UrlTestStandard_RTT = 0
+const UrlTestStandard_Handshake = 1
+const UrlTestStandard_FisrtHandshake = 2
+
+var errNoRedir = errors.New("no redir")
+
+func UrlTest(client *http.Client, link string, timeout int32, standard int) (int32, error) {
 	if client == nil {
 		return 0, fmt.Errorf("no client")
 	}
 	defer client.CloseIdleConnections()
 
-	// Test handshake time
-	var time_start time.Time
-	var times = 1
-	var rtt_times = 1
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return errNoRedir
+	}
 
-	// Test RTT "true delay"
-	if link2 := strings.TrimLeft(link, "true"); link != link2 {
-		link = link2
-		times = 3
-		rtt_times = 2
+	var time_start time.Time
+	var hsk_end time.Time
+	var time_end time.Time
+	var times int
+
+	switch standard {
+	case UrlTestStandard_FisrtHandshake:
+		times = 1
+	case UrlTestStandard_Handshake:
+		times = 2
+		rt := client.Transport.(*http.Transport)
+		rt.DisableKeepAlives = true
+	case UrlTestStandard_RTT:
+		times = 2
+	default:
+		return 0, errors.New("unknown urltest standard")
 	}
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Millisecond)
@@ -35,19 +53,41 @@ func UrlTest(client *http.Client, link string, timeout int32) (int32, error) {
 		return 0, err
 	}
 
-	for i := 0; i < times; i++ {
-		if i == 1 || times == 1 {
-			time_start = time.Now()
-		}
+	trace := &httptrace.ClientTrace{
+		TLSHandshakeDone: func(cs tls.ConnectionState, err error) {
+			hsk_end = time.Now()
+		},
+		GotFirstResponseByte: func() {
+			time_end = time.Now()
+		},
+		WroteHeaders: func() {
+			hsk_end = time.Now()
+		},
+	}
+	req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
 
+	for i := 0; i < times; i++ {
+		time_start = time.Now()
 		resp, err := client.Do(req)
 		if err != nil {
-			return 0, err
+			if errors.Is(err, errNoRedir) {
+				err = nil
+			} else {
+				return 0, err
+			}
 		}
 		resp.Body.Close()
 	}
 
-	return int32(time.Since(time_start).Milliseconds() / int64(rtt_times)), nil
+	if time_end.IsZero() {
+		time_end = time.Now()
+	}
+
+	if standard == UrlTestStandard_RTT {
+		time_start = hsk_end
+	}
+
+	return int32(time_end.Sub(time_start).Milliseconds()), nil
 }
 
 func TcpPing(address string, timeout int32) (ms int32, err error) {
